0370835e4f24e0e95047ca21d5aff3e2c674f297
Yuliya Raikova <yuradev2001@gmail.com>
2023-01-04 11:01:03 +0300

    .

--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -3,12 +3,17 @@ package jdbc;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.sql.DataSource;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 @Getter
 @Setter
-public class CustomDataSource {
+public class CustomDataSource implements DataSource {
     private static volatile CustomDataSource instance;
     private final String driver;
     private final String url;
@@ -38,5 +43,50 @@ public class CustomDataSource {
                             prop.getProperty("postgres.name", ""));
         }
         return instance;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
     }
 }
